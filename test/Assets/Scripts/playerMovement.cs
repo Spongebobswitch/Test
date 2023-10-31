e4d3fecbeb0285dbe2e30c623da6591b35811e46
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2023-10-31 11:39:05 -0700

    update scene and player controller

--- test/Assets/Scripts/playerMovement.cs
+++ test/Assets/Scripts/playerMovement.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    private Vector3 moveDirection;
+    private int speed = 10;
     // Start is called before the first frame update
     void Start()
     {
         //the player controller is done
         print("the player controller is complete YAY");
+    }
+    public void jump()
+    {
+
+    }
+    public void Move()
+    {
+       
     }
 
     // Update is called once per frame
