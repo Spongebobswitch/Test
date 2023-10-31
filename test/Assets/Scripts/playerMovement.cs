f9a6c9aaa8d40225ca76fb9af0068f41c28ce8ef
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2023-10-31 11:27:15 -0700

    player controller
    the controller is done YAAAAAAAAAAAY

---
+++ test/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //the player controller is done
+        print("the player controller is complete YAY");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
